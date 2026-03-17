9c3f8804c26a5f1b1e96eac46ccd1a99e0ee613d
concario17-max <concario17@gmail.com>
2026-03-17 09:10:27 +0900

    Fix global player motion import

--- src/components/Player/VolumeControl.jsx
+++ src/components/Player/VolumeControl.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import { Volume2, VolumeX } from 'lucide-react';
 
 const VolumeControl = ({ isMuted, volume, toggleMute, handleVolumeSeek }) => {
